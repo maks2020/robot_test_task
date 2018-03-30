8239bf00987352ab4651d41745135e3844fe6c0a
maxim spiridonov <spiridonov.maxim@simbirsoft.com>
2018-03-30 11:30:10 +0400

    added class Client, method .get_balance_positive() DataBase minor changes

--- sut/clients.py
+++ sut/clients.py
@@ -18,14 +18,13 @@ class DataBase:
         return conn, cur
 
     def count_db(self, name_tbl):
-        query_count = self.cur.execute('select count(*) from {}'.format(name_tbl))
+        query_count = self.cur.execute('SELECT COUNT(*) FROM {}'.format(name_tbl))
         count,  = query_count.fetchone()
         return count
 
-    def add_row(self, *args, row_id=None):
-        if row_id is not None:
-            self.cur.execute('insert into BALANCES() values(?,?)', args)
-            self.conn.commit()
+    def add_row(self, name_tbl, *args):
+        self.cur.execute('INSERT INTO {} VALUES(?,?)'.format(name_tbl), *args)
+        self.conn.commit()
 
     def close_db(self):
         self.conn.close()
@@ -33,5 +32,25 @@ class DataBase:
     def __del__(self):
         self.close_db()
 
+
+class Client:
+    def __init__(self):
+        self._id_client_positive = ''
+
+    def get_balance_positive(self):
+        db = DataBase()
+        query_balances = db.cur.execute('SELECT * FROM BALANCES WHERE BALANCE > 0')
+        client_tpl = query_balances.fetchone()
+        if client_tpl:
+            self._id_client_positive = client_tpl[0]
+            return client_tpl
+        count_cliens_db = db.count_db('CLIENTS')
+        client_id_new = count_cliens_db + 1
+        client_tpl = (client_id_new, 'Client_{}'.format(client_id_new))
+        db.add_row('CLIENTS', client_tpl)
+        balance_tpl = (client_id_new, 10.5)
+        db.add_row('BALANCES', balance_tpl)
+        self._id_client_positive = client_id_new
+        return client_tpl
 
 
