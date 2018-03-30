23d5d6693960c7e23ec621f77201ad4c9009b5c2
maxim spiridonov <spiridonov.maxim@simbirsoft.com>
2018-03-30 15:55:36 +0400

    clients.Client added methods set_client_service, get_connect_to_db changed __init__, get_balance_positive, get_client_services, get_services, get_ex_services DataBase add connect, changed __init__()

--- sut/clients.py
+++ sut/clients.py
@@ -1,5 +1,6 @@
 import os
-import json
+import random
+import time
 
 import sqlite3 as lite
 import requests
@@ -11,13 +12,12 @@ DATABASE_PATH = os.path.join(BASE_DIR, 'web', 'clients.db')
 class DataBase:
     def __init__(self, db_path=DATABASE_PATH):
         self.db_path = db_path
-        self.conn = lite.connect(self.db_path)
-        self.cur = self.conn.cursor()
+        self.conn = None
+        self.cur = None
 
     def connect(self):
-        conn = lite.connect(self.db_path)
-        cur = conn.cursor()
-        return conn, cur
+        self.conn = lite.connect(self.db_path)
+        self.cur = self.conn.cursor()
 
     def count_db(self, name_tbl):
         query_count = self.cur.execute('SELECT COUNT(*) FROM {}'.format(name_tbl))
@@ -37,61 +37,76 @@ class DataBase:
 
 class Client:
     def __init__(self):
-        self.client_data_positive = ''
-        self.client_services = ''
-        self.services = ''
-        self.diff_services = ''
+        self.db = None
+        self.cur = None
+        self.id_client_positive = None
+        self.client_services = None
+        self.services = None
+        self.id_service = None
+        self.cost_service = None
+        self.headers = {'Content-Type': 'application/json'}
+
+    def get_connect_to_db(self):
+        self.db = DataBase()
+        self.db.connect()
+        self.cur = self.db.cur
 
     def get_balance_positive(self):
-        db = DataBase()
-        query_balances = db.cur.execute('SELECT * FROM BALANCES WHERE BALANCE > 0')
+        query_balances = self.cur.execute('SELECT * FROM BALANCES WHERE BALANCE > 0')
         client_tpl = query_balances.fetchone()
         if client_tpl:
-            self.client_data_positive = client_tpl
+            self.id_client_positive, _ = client_tpl
             return client_tpl
-        count_cliens_db = db.count_db('CLIENTS')
-        client_id_new = count_cliens_db + 1
+        count_clients_db = self.db.count_db('CLIENTS')
+        client_id_new = count_clients_db + 1
         client_tpl = (client_id_new, 'Client_{}'.format(client_id_new))
-        db.add_row('CLIENTS', client_tpl)
+        self.db.add_row('CLIENTS', client_tpl)
         balance_tpl = (client_id_new, 10.5)
-        db.add_row('BALANCES', balance_tpl)
-        self.client_data_positive = client_tpl
+        self.db.add_row('BALANCES', balance_tpl)
+        self.id_client_positive, _ = client_tpl
         return client_tpl
 
     def get_client_services(self, port):
-        data = {}
-        data['client_id'], _ = self.client_data_positive
-        headers = {'Content-Type': 'application/json'}
+        data = {'client_id': self.id_client_positive}
         url = 'http://localhost:{port}/client/services'.format(port=port)
-        response = requests.post(url, headers=headers, json=data)
-        client_services = response.json()
-        self.client_services = client_services
-        return client_services
+        response = requests.post(url, headers=self.headers, json=data)
+        client_services_ = response.json()
+        self.client_services = client_services_
+        return client_services_
 
     def get_services(self, port):
         url = 'http://localhost:{port}/services'.format(port=port)
-        headers = {'Content-Type': 'application/json'}
-        response = requests.get(url, headers=headers)
-        services = response.json()
-        self.services = services
-        return services
+        response = requests.get(url, headers=self.headers)
+        services_ = response.json()
+        self.services = services_
+        return services_
 
     def get_ex_services(self):
         if self.client_services['count'] != self.services['count']:
             diff_services = [item for item in self.services['items'] if
                              item not in self.client_services['items']]
-            self.diff_services = diff_services
-            return diff_services
+            ex_service = random.choice(diff_services)
+            self.id_service = ex_service['id']
+            self.cost_service = ex_service['cost']
+            return ex_service
+
+    def set_client_service(self, port):
+        if self.id_service is not None:
+            url = 'http://localhost:{port}/client/add_service'.format(port=port)
+            data = {'client_id': self.id_client_positive, 'service_id': self.id_service}
+            response = requests.post(url, headers=self.headers, json=data)
+            return response.status_code
 
 
 if __name__ == '__main__':
     client = Client()
+    client.get_connect_to_db()
     balance_positive = client.get_balance_positive()
     print(balance_positive)
     client_services = client.get_client_services(5000)
     services = client.get_services(5000)
     client.get_ex_services()
-    if client.diff_services:
-        client_services['count'] += len(client.diff_services)
-        client_services['items'] += client.diff_services
-        print(client_services)
+    print(client.id_service)
+    client.set_client_service(5000)
+    client_services = client.get_client_services(5000)
+    print(client_services)
