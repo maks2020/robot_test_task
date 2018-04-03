dd6b84e450d16d3d15847c788230634e8410a4c4
maxim spiridonov <spiridonov.maxim@simbirsoft.com>
2018-04-03 16:41:17 +0400

    ClientLibrary is changed

--- library/ClientLibrary.py
+++ library/ClientLibrary.py
@@ -41,30 +41,16 @@ class ClientLibrary(DataBase):
 
     def __init__(self):
         super(ClientLibrary, self).__init__()
-        self._id_client_positive = None
-        self._balance_client_positive = None
-        self._client_services = None
-        self._services = None
-        self._id_service = None
-        self._cost_service = None
-        self._balance = None
-        self._status = None
 
     def connect_to_db(self):
         self.connect = sqlite3.connect(self._db_path)
         self.cursor = self.connect.cursor()
-        try:
-            self.cursor.execute('SELECT * FROM BALANCES')
-        except sqlite3.OperationalError:
-            raise sqlite3.OperationalError('not connect database for task')
 
     def get_client_with_positive_balance(self):
-        self._status = None
         client_balance_query = self.cursor.execute('SELECT * FROM BALANCES'
                                                    ' WHERE BALANCE > 0 LIMIT 1')
         client_balance = client_balance_query.fetchone()
         if client_balance:
-            self._id_client_positive, self._balance_client_positive = client_balance
             return client_balance
         else:
             count_clients_row = self.count_row('CLIENTS')
@@ -73,83 +59,77 @@ class ClientLibrary(DataBase):
             self.add_client(client_data)
             client_balance = (client_id_new, 5.0)
             self.add_client_balance(client_balance)
-            self._id_client_positive, self._balance_client_positive = client_balance
         return client_balance
 
-    def get_client_services(self, url):
-        data = {'client_id': self._id_client_positive}
+    def get_client_services(self, url, client_balance):
+        id_client, _ = client_balance
+        data = {'client_id': id_client}
         url = urljoin(url, 'client/services')
         response = requests.post(url, headers=ClientLibrary.HEADERS, json=data)
         assert response.status_code == 200
         client_services_ = response.json()
-        self._client_services = client_services_
+        # self._client_services = client_services_
         return client_services_
 
     def get_services(self, url):
         url_services = urljoin(url, 'services')
         response = requests.get(url_services, headers=ClientLibrary.HEADERS)
         services_ = response.json()
-        self._services = services_
         assert response.status_code == 200
         return services_
 
-    def get_unused_services(self):
-        try:
-            assert self._client_services
-        except AssertionError:
-            raise ValueError('client services not define')
-        try:
-            assert self._services
-        except AssertionError:
-            raise ValueError('services not define')
-        if self._client_services['count'] != self._services['count']:
-            diff_services = [item for item in self._services['items']
-                             if item not in self._client_services['items']]
+    def get_unused_services(self, client_services, services):
+        if client_services['count'] != services['count']:
+            diff_services = [item for item in services['items']
+                             if item not in client_services['items']]
             unused_service = random.choice(diff_services)
             assert unused_service
-            self._id_service = unused_service['id']
-            self._cost_service = unused_service['cost']
             return unused_service
 
-    def set_client_service(self, url):
-        if self._id_service:
+    def set_client_service(self, url, client_balance, unused_service):
+        id_client, _ = client_balance
+        id_service = unused_service['id']
+        if id_service:
             url = urljoin(url, 'client/add_service')
-            data = {'client_id': self._id_client_positive, 'service_id': self._id_service}
+            data = {'client_id': id_client, 'service_id': id_service}
             response = requests.post(url, headers=ClientLibrary.HEADERS, json=data)
             assert response.status_code == 202
             return response.status_code
 
-    def wait_new_service(self, url):
+    def wait_new_service(self, url, client_balance, unused_service):
+        id_client, _ = client_balance
+        id_service = unused_service['id']
         start_time = datetime.now()
         while True:
-            client_services_ = self.get_client_services(url)
+            client_services_ = self.get_client_services(url, client_balance)
             id_services_lst = [item['id'] for item in client_services_['items']]
-            if self._id_service in id_services_lst:
-                print('Service id = {} added'.format(self._id_service))
+            if id_service in id_services_lst:
                 return client_services_
             delta_time = datetime.now() - start_time
             if delta_time.seconds >= 60:
                 if client_services_['count'] == 0:
                     message = ('The client id {} does not have services. '
                                'The client possible does not exist in database'
-                               .format(self._id_client_positive))
+                               .format(id_client))
                 else:
                     message = 'Something went wrong'
                 raise TimeoutError('Exceeded waiting time request... {message}'
                                    .format(message=message))
             time.sleep(5)
 
-    def get_client_balance(self):
+    def get_client_balance(self, client_balance):
+        id_client, _ = client_balance
         query_balance = self.cursor.execute('SELECT BALANCE FROM BALANCES '
                                             'WHERE CLIENTS_CLIENT_ID=?',
-                                            (self._id_client_positive,))
+                                            (id_client,))
         balance, = query_balance.fetchone()
         assert balance
-        self._balance = balance
         return balance
 
-    def compare_start_end_balance(self):
-        balance_end = self._balance_client_positive - self._cost_service
-        BuiltIn().run_keyword('Should Be Equal', balance_end, self._balance,
-                              "Expected status to be '{expected}' but was '{status}'."
-                              .format(expected=balance_end, status=self._balance))
+    def compare_start_end_balance(self, client_balance, unused_service, current_balance):
+        _, start_balance = client_balance
+        cost_service = unused_service['cost']
+        balance_end = start_balance - cost_service
+        BuiltIn().should_be_equal(balance_end, current_balance,
+                                  "Expected status to be '{expected}' but was '{status}'."
+                                  .format(expected=balance_end, status=current_balance))
